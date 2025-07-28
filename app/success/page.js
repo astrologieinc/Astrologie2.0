6a4f91501647260177f5ce8e68ea85d46290535d
astrologieinc <astrologieinc@gmail.com>
2025-07-28 16:41:37 -0400

    Add success page for form submissions

---
+++ app/success/page.js
@@ -0,0 +1,10 @@
+export default function Success() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Thank You!</h1>
+      <p>Your astrology consultation request has been received.</p>
+      <p>We'll get back to you soon!</p>
+      <a href="/" style={{ color: '#0070f3' }}>Back to Home</a>
+    </div>
+  );
+}
