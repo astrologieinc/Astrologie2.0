9690fbe3986983cfd482b991d4392d909b76b93e
astrologieinc <astrologieinc@gmail.com>
2025-07-28 16:43:53 -0400

    Fix success page syntax errors

--- app/success/page.js
+++ app/success/page.js
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 export default function Success() {
   return (
     <div style={{ padding: '40px', textAlign: 'center' }}>
       <h1>Thank You!</h1>
       <p>Your astrology consultation request has been received.</p>
-      <p>We'll get back to you soon!</p>
-      <a href="/" style={{ color: '#0070f3' }}>Back to Home</a>
+      <p>We&apos;ll get back to you soon!</p>
+      <Link href="/" style={{ color: '#0070f3' }}>Back to Home</Link>
     </div>
   );
 }
